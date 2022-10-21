1c581a695d05e6f6aa854d535a3ca692103c3f12
Divyanshu Shukla <100156820+Divyanshu6566@users.noreply.github.com>
2022-10-21 23:31:17 +0530

    For loop problem in java

---
+++ For_loop_problem.java
@@ -0,0 +1,8 @@
+public class java_17_for_loop {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<n; i++){
+            System.out.println(2*i);  // It will print 0 to 4 in multiple of 2
+        }
+    }
+}
